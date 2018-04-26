ccd96036f3652bdf5c9ac6d31cc6a91a00f5fe79
Andrew Pham <andrew.w.pham@gmail.com>
2018-04-26 18:59:36 -0400

    completed working prototype

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,22 +1,80 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function firstComponent() {
-	return(
-		<p>Guy is a cat! 🐱</p>
-	);
-}
 
-const secondComponent = () => {
+
+const SecondComponent = () => {
 	return (
 		<p>Bobo is a dog! 🐶</p>
 	)
+};
+
+const MasterComponent = () => {
+	const greeting = 'I am a string!';
+	return(
+		<div>
+
+			<p>{greeting}</p>
+			<PlayButton />
+			<SecondComponent />
+		</div>
+	);
 }
 
-// const masterComponent = () => {
-// 	return(
-		
-// 	);
-// }
+function PlayButton(props) {
+	const className = props.isMusicPlaying ? 'play active' : 'play';
+	return <a onClick={props.onClick} href="#" title="Play video" className={className} />;
+}
 
-ReactDOM.render(<firstComponent />, document.getElementById('hook'));
+class Container extends React.Component {
+	constructor() {
+		super();
+		this.state = { 
+			isMusicPlaying: false 
+		}
+		
+	}
+
+	handleClick() {
+		// if (this.state.isMusicPlaying) {
+		// 	this.setState({ isMusicPlaying: false });
+		// }
+		// else {
+		// 	this.setState({ isMusicPlaying: true });
+		
+		// }
+
+		if (this.state.isMusicPlaying) {
+			this.audio.pause();
+		}
+		else {
+			this.audio.play();
+		}
+
+		this.setState(prevState => {
+			return {
+				isMusicPlaying: !prevState.isMusicPlaying
+			};
+		});
+	};
+
+
+	render() {
+		const status = this.state.isMusicPlaying ? 'Playing' : 'Not Playing';
+		return (
+			<div>
+			
+				
+				<PlayButton 
+					onClick={this.handleClick.bind(this)}
+					isMusicPlaying={this.state.isMusicPlaying} 
+				/>
+				<audio id="audio" ref={(audioTag) => { this.audio = audioTag }} />
+			</div>
+		);
+	}
+}
+
+// const placeWeWantToPutComponent = document.getElementById('hook');
+ReactDOM.render(<Container />, document.getElementById('hook'));
+
